d0f18af428cc30637303fdc5f63396c049cafbba
Emily <emily.rocha192@gmail.com>
2024-05-14 08:20:50 -0300

    criando fundo

--- jogo_pega-coisas.py
+++ jogo_pega-coisas.py
@@ -7,3 +7,7 @@ pygame.init()
 tela = pygame.display.set_mode((800,500))
 pygame.display.set_caption("Jogo das princesas")
 tela.fill((80,120,200))
+
+FUNDO = pygame.image.load("imagens/princesas_mundo.png")
+FUNDO = pygame.transform.scale(FUNDO,(800,500))
+
