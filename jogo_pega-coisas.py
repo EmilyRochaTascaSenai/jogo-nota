7aeeb9f12fd2c6f2055eb5c8fe4d178aabf76790
Emily <emily.rocha192@gmail.com>
2024-05-14 10:51:11 -0300

    lista de fruta

--- jogo_pega-coisas.py
+++ jogo_pega-coisas.py
@@ -1,7 +1,6 @@
 import pygame
 from personagens import Personagem
 from obtsaculo import Obstaculo
-
 pygame.init()
 
 #Constrindo a tela
@@ -15,7 +14,15 @@ FUNDO = pygame.transform.scale(FUNDO,(800,500))
 
 #criando personagem
 bela= Personagem("imagens/bela.png",100,100,200,300)
-abacaxi=Obstaculo("Imagens/abacaxi.png",100,100,850,850)
+lista_fruta = [Obstaculo("imagens/abacaxi.png",100,50,850,60),
+               Obstaculo("imagens/abobora.png",100,50,850,120),
+               Obstaculo("imagens/banana.png",100,50,850,185),
+               Obstaculo("imagens/cerejas.png",100,50,850,265),
+               Obstaculo("imagens/laranja.png",100,50,850,330),
+               Obstaculo("imagens/limao.png",100,50,850,400),
+               Obstaculo("imagens/limao2.png",100,50,850,60)]
+
+
 #Criando um relogio para controlar o FPS
 clock = pygame.time.Clock()
 rodando = True
@@ -30,8 +37,8 @@ while rodando:
     #desenhando as imagens
     bela.movimentar_via_controle(pygame.K_RIGHT,pygame.K_LEFT)
     bela.desenhar(tela)
-    abacaxi.movimentar_via_controle(pygame.K_UP,pygame.K_DOWN,pygame.K_RIGHT,pygame.K_LEFT)
-    abacaxi.desenhar(tela)
+    
+    
     #configurando a fonte
     fonte=pygame.font.SysFont("arial black ",12)
     texto_pontucao_bela=fonte.render("Pontuação da bela",False,(255,203,219))
