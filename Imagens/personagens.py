b549c983b16c22aec20389b50d4d03c5d1e4d180
Emily <emily.rocha192@gmail.com>
2024-05-14 08:27:48 -0300

    class personagem

--- Imagens/personagens.py
+++ Imagens/personagens.py
@@ -1,1 +1,18 @@
 import pygame
+
+class Personagem:
+    def __init__(self,arquivo_imagem,largura_imagem,altura_imagem,x_inicial,y_inicial):
+        self.imagem = pygame.image.load(arquivo_imagem)
+
+        self.largura = largura_imagem
+        self.altura = altura_imagem
+
+        self.imagem = pygame.transform.scale(self.imagem,(self.largura,self.altura))
+
+        self.pos_x = x_inicial
+        self.pos_y = y_inicial
+
+        self.mascara=pygame.mask.from_surface(self.imagem)
+
+    def desenhar(self, tela):
+        tela.blit(self.imagem,(self.pos_x,self.pos_y))
